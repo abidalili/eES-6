deca35ff8085b943a1434602f22c396ab45d235d
abidalili <abidalili1995@gmail.com>
2023-03-11 17:09:33 +0200

    ES6 variable string concating and function declaration

---
+++ ES-6 variable and function/index.js
@@ -0,0 +1,30 @@
+//string concating
+
+// let x = 25;
+// let y = 30;
+
+// let sum = x + y;
+// console.log(`sum is ${sum} . the end`);
+
+// //es6 function declaration
+
+// //es5 was
+
+// function add(x, y) {
+//   var sum = x + y;
+//   console.log(sum);
+// }
+// add(20, 30);
+
+//es6
+// const functionname = (para1, para2) => {
+//   let sum = x + y;
+//   console.log(sum);
+// };
+// add(para1, para2);
+
+const add = (x, y) => {
+  let sum = x + y;
+  console.log(sum);
+};
+add(20, 50);
