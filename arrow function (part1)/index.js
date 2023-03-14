581aea291abc62ebffa3d9c0413216a1444484ce
abidalili <abidalili1995@gmail.com>
2023-03-15 01:00:34 +0200

    arrrow function part 1

---
+++ arrow function (part1)/index.js
@@ -0,0 +1,37 @@
+// js arrow function
+
+// js traditional function
+
+// function myFunction() {
+//   console.log("hello");
+// }
+// myFunction();
+
+// arrow function
+
+// const myFunction2 = () => {
+//   console.log("hello");
+// };
+// myFunction2();
+
+// REturning value traditional
+
+// function message1() {
+//   return "hi, im lili";
+// }
+// console.log(message1());
+
+// // REturning value arrow
+
+// const message2 = () => "hi im lili 2";
+// console.log(message2());
+
+//parameter pass traditional
+// function add1(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(add1(10, 20));
+
+//parameter pass arrow
+const add2 = (num1, num2) => num1 + num2;
+console.log(add2(10, 20));
