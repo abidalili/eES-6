9e593e8738e507d1f8da9a854e6ed3e24be619f0
abidalili <abidalili1995@gmail.com>
2023-03-17 23:03:42 +0200

    string method

---
+++ string methods- startWith,endsWith,indcludes/index.js
@@ -0,0 +1,20 @@
+//string Method
+// startWith(searchstring, position) = check a string starts with another string.The startsWith() method returns true if a string starts with a specified string.
+
+//Otherwise it returns false.
+
+//The startsWith() method is case sensitive.
+
+// let text = "Hello world, welcome to the universe.";
+// let result = text.startsWith("Hello");
+// console.log(result);
+
+//endsWith(searchstring, length) = check a string ends with another string
+//The endsWith() method returns true if a string ends with a specified string. Otherwise it returns false. The endsWith() method is case sensitive.
+// let text = "Hello world";
+// let result = text.endsWith("world");
+// console.log(result);
+
+//The includes() method returns true if a string contains a specified string.Otherwise it returns false.The includes() method is case sensitive.
+let text = "Hello world, welcome to the universe.";
+console.log(text.includes("world"));
