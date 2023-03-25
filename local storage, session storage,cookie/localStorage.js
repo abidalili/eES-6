9b03f0d7680b7798110268c8572b8d43e2115afb
abidalili <abidalili1995@gmail.com>
2023-03-25 03:14:08 +0200

    local storage and sassion storage

---
+++ local storage, session storage,cookie/localStorage.js
@@ -0,0 +1,35 @@
+//localStorage,sessionStorage and cookie
+//local storage store data as key value pair-string
+//setItem(key,value)
+//console.log(localStorage);
+
+//set(sending) data to storage
+/*localStorage.setItem("userName", "abida aktar");
+localStorage.setItem("password", "112423536");*/
+
+//get data from local storage
+//getItem(key)
+/*const userName = localStorage.getItem("userName");
+const password = localStorage.getItem("password");
+console.log(userName, password);*/
+
+//update data
+
+/*localStorage.setItem("userName", "lili aktar");
+localStorage.setItem("password", "16666662423536");*/
+
+//localStorage.clear(); //this item delete everything from local storage. nothing left
+//remove data
+
+/*localStorage.removeItem("userName");
+localStorage.removeItem("password");*/
+
+//setItem(key,value)
+//set(sending) data to storage
+/*const countries = ["Australia", "Finland", "Bangladesh"];
+localStorage.setItem("countries", JSON.stringify(countries));*/
+
+//getItem(key);
+
+/*const countriesList = JSON.parse(localStorage.getItem("countries"));
+console.log(countriesList);*/
